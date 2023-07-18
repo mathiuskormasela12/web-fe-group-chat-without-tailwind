3b69002a71c047b8244494539b0d9ab219cc7ef3
Mathius <mathiuskormasela12@gmail.com>
2023-07-18 21:00:32 +0700

    fix responsive create account page

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -3,8 +3,10 @@
 import styled from 'styled-components'
 
 export const Row = styled.div`
-  padding: 3rem 0 2rem 0;
+  padding: 3rem 0;
   min-height: 100vh;
+  display: flex;
+  align-items: center;
 `
 
 export const Col = styled.div`
